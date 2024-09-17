52809cd6b997d61d9653856edab3e0c74891062c
Mandeep Singh <mandeep6862@gmail.com>
2024-09-18 00:49:53 +0530

    \n case handle

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.css'
 })
 export class CalculatorComponent {
+  inputString:any="";
+  outputString:any;
+
+  submit(){
+    if(this.inputString == "" || this.inputString == null || this.inputString == undefined){
+      this.outputString = 0;
+    }
+    let arr = this.inputString;
+    if(arr.includes('n')){
+      arr=arr.split('\n')[0];
+      arr=arr.replace(/\\n/g, ",");
+    }
+    if(arr.includes(',')){
+      let tmparr:any[] = arr.split(",");
+      const sum = tmparr.reduce((accumulator, currentValue) => {
+        return Number(accumulator) + Number(currentValue);
+      }, 0);
+      this.outputString = sum;
+    }
+    else{
+      this.outputString = arr;
+    }
+    
+  }
+
+  onClickInput(){
+    this.outputString = "";
+  }
 
 }
